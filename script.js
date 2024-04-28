1db39ac28a55fd130897832861fa6c7fe2dc03b5
Darrion <68414637+itsdoble@users.noreply.github.com>
2024-04-28 11:51:42 +0530

    Added local storage peristence.

--- script.js
+++ script.js
@@ -1,32 +1,51 @@
 const link =
   "https://m.webtoons.com/en/action/viral-hit/ep-59-i-expected-nothing-out-of-the-ordinary-to-happen/comment?title_no=2268&episode_no=";
 
+if (!localStorage.getItem(1)) {
+  localStorage.setItem(1, JSON.stringify({ id: 2268, chapter: 1 }));
+}
+let details = JSON.parse(localStorage.getItem(1));
+console.log(details);
+
 const idInput = document.querySelector("input#id");
+idInput.placeholder = details.id;
 const chapterInput = document.querySelector("input#chapter");
+chapterInput.placeholder = details.chapter;
 const readButton = document.querySelector("#read");
 const nextButton = document.querySelector("#next");
 const current = document.querySelector("p");
-let chapter = 1;
-current.innerText = `Current: ${chapter} | Next: ${chapter + 1}`;
+current.innerText = `Current: ${details.chapter} | Next: ${
+  details.chapter + 1
+}`;
+idInput.onchange = () => {
+  details.id = +idInput.value;
+  localStorage.setItem(1, JSON.stringify(details));
+};
 readButton.onclick = () => {
   if (chapterInput.value) {
-    chapter = +chapterInput.value;
+    details.chapter = +chapterInput.value;
   }
-  current.innerText = `Current: ${chapter} | Next: ${chapter + 1}`;
+  current.innerText = `Current: ${details.chapter} | Next: ${
+    details.chapter + 1
+  }`;
   window.open(
     `https://m.webtoons.com/en/action/viral-hit/ep-59-i-expected-nothing-out-of-the-ordinary-to-happen/comment?title_no=${
       idInput.value ? idInput.value : 2268
-    }&episode_no=${chapter}`,
+    }&episode_no=${details.chapter}`,
     "_blank"
   );
 };
 nextButton.onclick = () => {
-  chapter = chapter + 1;
-  current.innerText = `Current: ${chapter} | Next: ${chapter + 1}`;
+  details.chapter = details.chapter + 1;
+  chapterInput.placeholder = details.chapter;
+  localStorage.setItem(1, JSON.stringify(details));
+  current.innerText = `Current: ${details.chapter} | Next: ${
+    details.chapter + 1
+  }`;
   window.open(
     `https://m.webtoons.com/en/action/viral-hit/ep-59-i-expected-nothing-out-of-the-ordinary-to-happen/comment?title_no=${
       idInput.value ? idInput.value : 2268
-    }&episode_no=${chapter}`,
+    }&episode_no=${details.chapter}`,
     "_blank"
   );
 };
